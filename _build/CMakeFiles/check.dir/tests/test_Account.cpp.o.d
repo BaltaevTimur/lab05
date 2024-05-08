424d2e8a323272e004180f6bf0e2bd16845dce3a
BaltaevTimur <timur.baltaev.2005@gmail.com>
2024-05-08 21:28:06 +0300

    added gtest

---
+++ _build/CMakeFiles/check.dir/tests/test_Account.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/check.dir/tests/test_Account.cpp.o: \
+ /home/timur/BaltaevTimur/workspace/lab05/lab05/tests/test_Account.cpp \
+ /usr/include/stdc-predef.h
---
+++ _build/CMakeFiles/check.dir/tests/test_Transaction.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/check.dir/tests/test_Transaction.cpp.o: \
+ /home/timur/BaltaevTimur/workspace/lab05/lab05/tests/test_Transaction.cpp \
+ /usr/include/stdc-predef.h
